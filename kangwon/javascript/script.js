e1fa3a0c1e03a75601f7e62442ef2467d75091f0
kwon hyeonseong <goodsosbva@carang.co.kr>
2023-06-22 13:35:45 +0900

    강원 천문대 사이트만들기 웹디자인 기능사 (2/2) 완

--- kangwon/javascript/script.js
+++ kangwon/javascript/script.js
@@ -16,4 +16,11 @@ jQuery(document).ready(function(){
             .end()
             .appendTo(".imgslide");
     }, 3000)
+
+    $(".notice li:first").click(function () {
+        $("#modal").addClass("active");
+      });
+      $(".btn").click(function () {
+        $("#modal").removeClass("active");
+      });
 });
