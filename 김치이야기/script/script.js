c300a43661dd5410caeb546631887706e4eb90ab
khs <hs.kwan@okestro.com>
2025-12-02 22:23:48 +0900

    feat: complete 김치야기 web desigin project

--- 김치이야기/script/script.js
+++ 김치이야기/script/script.js
@@ -1,6 +1,5 @@
 function solution() {
 	const menu = document.querySelectorAll(".smenu");
-	console.log('menu >> ', menu)
 
 	menu.forEach(function(item) {
 		item.addEventListener("mouseover", function() {
@@ -12,6 +11,45 @@ function solution() {
 			const smenu = item.querySelector(".submenus");
 			smenu.style.display = "none";
 		})
+	})
+    
+    let idx = 0;
+	const slideItems = document.querySelectorAll(".slide");
+	const slidesContainer = document.querySelector(".slides");
+	const total = slideItems.length;
+
+	function applySlideTransform() {
+			for (let i = 0; i < total; i++) {
+				const item = slideItems[i];
+			    item.style.transform = `translateY(-${idx * 100}%)`;
+	            item.style.transition = 'transform 0.5s ease-in-out';
+			}
+	}
+
+    applySlideTransform();
+
+	function slidesWindow () {
+		idx = (idx + 1) % total;
+
+	    for (let i = 0; i < total; i++) {
+	        const item = slideItems[i];
+	        item.style.transform = `translateY(-${idx * 100}%)`;
+	        item.style.transition = 'transform 0.5s ease-in-out';
+	    }
+	}
+
+	setInterval(slidesWindow, 1000);
+
+
+	const modalOpenItem = document.querySelector(".notice-title");
+    const modalItem = document.querySelector(".modal");
+	modalOpenItem.addEventListener("click", function() {
+		modalItem.style.display = "block";	
+	})
+
+	const cancelItem = document.querySelector(".modal-cancel");
+	cancelItem.addEventListener("click", function() {
+		modalItem.style.display = "none";
 	})
 }
 
